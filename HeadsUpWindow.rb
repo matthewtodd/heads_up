5caa692f45fa71ce92e06107d4ac17e1c424cc74
Matthew Todd <matthew.todd@gmail.com>
2009-01-16 11:59:41 +0300

    We can see a window!

--- HeadsUpWindow.rb
+++ HeadsUpWindow.rb
@@ -3,11 +3,11 @@ require 'osx/cocoa'
 class HeadsUpWindow < OSX::NSWindow
 	def	init
 		initWithContentRect_styleMask_backing_defer([100,100,600,200], OSX::NSBorderlessWindowMask, OSX::NSBackingStoreBuffered, false)
-		setContentView(HeadsUpTextView.alloc.initWithFrame(frame))
-		setAutodisplay(true)		
+		setAutodisplay(true)
 		setHasShadow(false)
 		setOpaque(false)
 		setReleasedWhenClosed(true)
+		orderFront(self)
 		self
 	end
 end

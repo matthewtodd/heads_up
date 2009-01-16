1e6cf1adabcbb067288d6775513638c878fe7d5e
Matthew Todd <matthew.todd@gmail.com>
2009-01-16 10:35:02 +0300

    Initial import

---
+++ main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  HeadsUp
+//
+//  Created by Matthew Todd on 1/16/09.
+//  Copyright (c) 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <RubyCocoa/RBRuntime.h>
+
+int main(int argc, const char *argv[])
+{
+    return RBApplicationMain("rb_main.rb", argc, argv);
+}

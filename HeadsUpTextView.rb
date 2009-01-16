51b68be2db0ad539e3b164277b1c5155ac1f7b5b
Matthew Todd <matthew.todd@gmail.com>
2009-01-16 13:25:18 +0300

    Setting textview properties, positioning window.

--- HeadsUpTextView.rb
+++ HeadsUpTextView.rb
@@ -1,4 +1,14 @@
 require 'osx/cocoa'
 
 class HeadsUpTextView < OSX::NSTextView
+  def initWithFrame(frame)
+    super_initWithFrame(frame)
+
+    setBackgroundColor(OSX::NSColor.clearColor)
+    setFont(OSX::NSFont.fontWithName_size('Monaco', 12.0))
+    setTextColor(OSX::NSColor.whiteColor.colorWithAlphaComponent(0.5))
+
+    setString('Hello, World!')
+    self
+  end
 end
--- HeadsUpWindow.rb
+++ HeadsUpWindow.rb
@@ -2,11 +2,15 @@ require 'osx/cocoa'
 
 class HeadsUpWindow < OSX::NSWindow
 	def	init
-		initWithContentRect_styleMask_backing_defer([100,100,600,200], OSX::NSBorderlessWindowMask, OSX::NSBackingStoreBuffered, false)
+		initWithContentRect_styleMask_backing_defer([12,12,600,18], OSX::NSBorderlessWindowMask, OSX::NSBackingStoreBuffered, false)
+		
 		setAutodisplay(true)
+    setBackgroundColor(OSX::NSColor.clearColor)
+		setContentView(HeadsUpTextView.alloc.initWithFrame(frame))
 		setHasShadow(false)
 		setOpaque(false)
 		setReleasedWhenClosed(true)
+		
 		orderFront(self)
 		self
 	end

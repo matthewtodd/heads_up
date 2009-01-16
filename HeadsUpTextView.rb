ffe2b156781cb1fd8169df0a33d50afc06497a4f
Matthew Todd <matthew.todd@gmail.com>
2009-01-16 14:09:28 +0300

    Keep window from Expose'ing, and bottom-justify text.

--- HeadsUpTextView.rb
+++ HeadsUpTextView.rb
@@ -6,9 +6,21 @@ class HeadsUpTextView < OSX::NSTextView
 
     setBackgroundColor(OSX::NSColor.clearColor)
     setFont(OSX::NSFont.fontWithName_size('Monaco', 12.0))
+    setString(`/Users/mtodd/Code/events/bin/events`)
     setTextColor(OSX::NSColor.whiteColor.colorWithAlphaComponent(0.5))
 
-    setString('Hello, World!')
     self
+  end
+
+  def setString(string)
+    super_setString(bottomJustify(string))
+  end
+
+  private
+
+  def bottomJustify(string)
+    lines = string.split(/\n/)
+    (12 - lines.size).times { lines.unshift('') }
+    lines.join("\n")
   end
 end
--- HeadsUpWindow.rb
+++ HeadsUpWindow.rb
@@ -2,12 +2,14 @@ require 'osx/cocoa'
 
 class HeadsUpWindow < OSX::NSWindow
 	def	init
-		initWithContentRect_styleMask_backing_defer([12,12,600,18], OSX::NSBorderlessWindowMask, OSX::NSBackingStoreBuffered, false)
+		initWithContentRect_styleMask_backing_defer([12,12,600,192], OSX::NSBorderlessWindowMask, OSX::NSBackingStoreBuffered, false)
 		
 		setAutodisplay(true)
     setBackgroundColor(OSX::NSColor.clearColor)
 		setContentView(HeadsUpTextView.alloc.initWithFrame(frame))
 		setHasShadow(false)
+		setIgnoresMouseEvents(true)
+		setLevel(OSX::KCGDesktopWindowLevel)
 		setOpaque(false)
 		setReleasedWhenClosed(true)
 		

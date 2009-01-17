a5f314582c147a8b03278a138ce372eafae51464
Matthew Todd <matthew.todd@gmail.com>
2009-01-17 13:17:55 +0300

    Roughly size windows based on font size.
    Also, fledgling right-justification support.

--- HeadsUpWindow.rb
+++ HeadsUpWindow.rb
@@ -1,6 +1,9 @@
 require 'osx/cocoa'
 
 class HeadsUpWindow < OSX::NSWindow
+  ROWS, COLS = 12, 80
+  FONT_HEIGHT, FONT_WIDTH = 16.0, 7.201171875 # FIXME chicken-and-egg keeps me from asking layoutManger.defaultLineHeightForFont. So resize window, textView, *after* initializing?
+
   def initWithLocation(location)
     initWithContentRect_styleMask_backing_defer(frame_for_location(location), OSX::NSBorderlessWindowMask, OSX::NSBackingStoreBuffered, false)
 
@@ -20,23 +23,32 @@ class HeadsUpWindow < OSX::NSWindow
   end
 
   def update_contents(string)
+    string = right_justify(string) if @location == :bottom_right
     contentView.setString(bottom_justify(string))
   end
 
   private
 
+  # FIXME this will blow up when there are more than ROWS lines
   def bottom_justify(string)
     lines = string.split(/\n/)
-    (12 - lines.size).times { lines.unshift('') }
+    (ROWS - lines.size).times { lines.unshift('') }
     lines.join("\n")
+  end
+
+  # FIXME this will blow up when there are more than COLS columns
+  def right_justify(string)
+    lines = string.split(/\n/)
+    width = lines.map { |line| line.length }.max
+    lines.map { |line| (' ' * (COLS - width)) + line }.join("\n")
   end
 
   def frame_for_location(location)
     main_frame = OSX::NSScreen.mainScreen.frame
 
-    margin = 12
-    width  = 600
-    height = 192
+    margin = FONT_HEIGHT
+    width  = COLS * FONT_WIDTH
+    height = ROWS * FONT_HEIGHT
 
     case location
     when :bottom_left

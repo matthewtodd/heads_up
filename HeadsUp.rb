2f0e8127cdb4c15755aa8f00cfa3afcc7b3b4b3f
Matthew Todd <matthew.todd@gmail.com>
2009-01-17 17:53:00 +0300

    Read preferences on startup.

--- HeadsUp.rb
+++ HeadsUp.rb
@@ -1,15 +1,14 @@
 require 'osx/cocoa'
 
 class HeadsUp < OSX::NSObject
-  def applicationDidChangeScreenParameters(aNotification)
-    @controllers.each { |controller| controller.update_window_position }
+  def applicationDidFinishLaunching(aNotification)
+    @controllers = [:bottom_left, :bottom_right].map do |location|
+      HeadsUpWindowController.alloc.initWithLocation(location)
+    end
+    OSX::NSNotificationCenter.defaultCenter.addObserver_selector_name_object(self, :applicationDidChangeScreenParameters, 'NSApplicationDidChangeScreenParametersNotification', nil)
   end
 
-  def applicationDidFinishLaunching(aNotification)
-    OSX::NSNotificationCenter.defaultCenter.addObserver_selector_name_object(self, :applicationDidChangeScreenParameters, 'NSApplicationDidChangeScreenParametersNotification', nil)
-
-    @controllers = []
-    @controllers << HeadsUpWindowController.alloc.initWithLocation_command(:bottom_left, '/Users/mtodd/Code/Inactive/events/bin/events 2009-03-29')
-    @controllers << HeadsUpWindowController.alloc.initWithLocation_command(:bottom_right, '/bin/date')
+  def applicationDidChangeScreenParameters(aNotification)
+    @controllers.each { |controller| controller.update_window_position }
   end
 end
--- HeadsUpWindowController.rb
+++ HeadsUpWindowController.rb
@@ -1,15 +1,17 @@
 require 'osx/cocoa'
 
 class HeadsUpWindowController < OSX::NSWindowController
-  def initWithLocation_command(location, command)
-    @command = command
+  def initWithLocation(location)
+    @location = location
     initWithWindow(HeadsUpWindow.alloc.initWithLocation(location))
+    initialize_user_defaults
+    read_command_from_user_defaults
     start_window_update_timer
     self
   end
 
   def update_window_contents
-    window.updateContents(`#{@command}`)
+    window.updateContents(`#{@command}`) unless @command.empty?
   end
 
   def update_window_position
@@ -17,6 +19,15 @@ class HeadsUpWindowController < OSX::NSWindowController
   end
 
   private
+
+  def initialize_user_defaults
+    @defaults = OSX::NSUserDefaults.standardUserDefaults
+    @defaults.registerDefaults(@location => { :command => '' })
+  end
+
+  def read_command_from_user_defaults
+    @command = @defaults.dictionaryForKey(@location).objectForKey(:command)
+  end
 
   def start_window_update_timer
     timer = OSX::NSTimer.scheduledTimerWithTimeInterval_target_selector_userInfo_repeats(60, self, :update_window_contents, nil, true)

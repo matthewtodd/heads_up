b89024b8970c10f4b3453eed4a24a25b70d5cb95
Matthew Todd <matthew.todd@gmail.com>
2010-11-24 12:51:22 +0300

    Note to future self.

--- HeadsUpAppDelegate.m
+++ HeadsUpAppDelegate.m
@@ -5,6 +5,15 @@
 
 @synthesize window;
 
+// TODO set up some default commands?
+//
+// Not as simple as it would seem, because values passed to
+// NSUserDefaults.registerDefaults are returned when the keys
+// have been set back to nil, as they are when the user clears
+// out one of the boxes in the preferences window. So, I need
+// some way to register the defaults only if they've never
+// been registered before... weird.
+//
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
 	left  = [[HeadsUpWindowController alloc] initWithScreen:[[HeadsUpScreen alloc] initWithKey:@"bottom_left"]];
 	right = [[HeadsUpWindowController alloc] initWithScreen:[[HeadsUpScreen alloc] initWithKey:@"bottom_right"]];
